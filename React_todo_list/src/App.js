d028f95d4e60021f081ea66c5b5016282f64ba38
AlaaSaleh7 <allaasaleeh7@gmail.com>
2021-05-24 01:41:37 +0300

    solve all Pulse Check

---
+++ React_todo_list/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import List from "./components/List";
+import ListItem from "./components/ListItem";
+
+// jsx
+
+const App = () => {
+  const todos = [
+    { todo: "wake up", id: 1 },
+    { todo: "breakfast", id: 2 },
+    { todo: "study", id: 3 },
+  ];
+  return (
+    <>
+      <h1>hello world</h1>
+       <List todoList = {todos}/>
+       <ListItem/>
+    </>
+  );
+};
+
+export default App;
---
+++ React_todo_list/src/components/ListItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const ListItem = ({todo,id})=>{
+    const handleClickMe = () => {
+        console.log({id})
+      };
+    return(
+        <> <ul><li  onClick={handleClickMe}> {todo} {id} </li></ul> 
+
+        </>
+    )
+}
+export  default ListItem;
